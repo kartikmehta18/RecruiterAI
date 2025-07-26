7a46badd598a1d68fceed48b60b90441aee8d36c
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2025-07-27 02:45:10 +0530

    making feedback api

--- services/Constants.jsx
+++ services/Constants.jsx
@@ -5,59 +5,59 @@
 //     Calender
 //  } from "lucide-react"
 
-import { LayoutDashboard, Settings, WalletCards, List,Component ,Calendar,Puzzle,User2Icon ,Code2Icon, BriefcaseBusinessIcon} from 'lucide-react';
+import { LayoutDashboard, Settings, WalletCards, List, Component, Calendar, Puzzle, User2Icon, Code2Icon, BriefcaseBusinessIcon } from 'lucide-react';
 
 
-export const SideBarOptions =[
+export const SideBarOptions = [
     {
-        name:"Dashboard",
-        icon:LayoutDashboard,
-        path:"/dashboard",
+        name: "Dashboard",
+        icon: LayoutDashboard,
+        path: "/dashboard",
     },
     {
-        name:"Schedule Interview",
-        icon:Calendar,
-        path:"/schedule-interview",
+        name: "Schedule Interview",
+        icon: Calendar,
+        path: "/schedule-interview",
     },
     {
-        name:"All Interview",
-        icon:List,
-        path:"/all-interview",
+        name: "All Interview",
+        icon: List,
+        path: "/all-interview",
     },
     {
-        name:"Billing",
-        icon:WalletCards,
-        path:"/billings",
+        name: "Billing",
+        icon: WalletCards,
+        path: "/billings",
     },
     {
-        name:"Settings",
-        icon:Settings,
-        path:"/settings",
+        name: "Settings",
+        icon: Settings,
+        path: "/settings",
     },
 ]
 
-export const InterviewType=[
+export const InterviewType = [
     {
-        title:"Technical",
-        icon:Code2Icon
+        title: "Technical",
+        icon: Code2Icon
     },
     {
-        title:"Behavioral",
-        icon:User2Icon
+        title: "Behavioral",
+        icon: User2Icon
     },
     {
-        title:"Experience",
-        icon:BriefcaseBusinessIcon
+        title: "Experience",
+        icon: BriefcaseBusinessIcon
     },
     {
-        title:"Problem Solving",
-        icon:Puzzle
+        title: "Problem Solving",
+        icon: Puzzle
     },
     {
-        title:"Leadership",
-        icon:Component
+        title: "Leadership",
+        icon: Component
     },
-    
+
 ]
 
 
@@ -88,3 +88,24 @@ interviewQuestions = [
 ]
 
 The goal is to create a structured, relevant, and time-optimized interview plan for a {{jobTitle}} role.`;
+
+
+export const FEEDBACK_PROMPT = `{{conversation}}
+Depends on this Interview Conversation between assistant and user,
+Give me feedback for user interview. Give me rating out of 10 for technical Skills,
+Communication, Problem Solving, Experience. Also give me summery in 3 lines
+about the interview and one line to let me know whether is recommanded
+for hire or not with msg. Give me response in JSON format
+{
+   feedback:{
+      rating:{
+         techicalSkills:5,
+         communication:6,
+         problemSolving:4,
+         experince:7
+      },
+      summery:<in 3 Line>,
+      Recommendation:"",
+      RecommendationMsg:""
+   }
+}`
